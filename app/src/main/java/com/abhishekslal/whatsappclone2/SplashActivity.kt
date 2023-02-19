543318535fa41af9696d106fb09806e2669de3c1
Abhishek S Lal <abhishek.s@aot-technologies.com>
2023-02-19 09:30:46 +0530

    minor changes

--- app/src/main/java/com/abhishekslal/whatsappclone2/SplashActivity.kt
+++ app/src/main/java/com/abhishekslal/whatsappclone2/SplashActivity.kt
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
+//    i) Lazy initialization was designed to prevent unnecessary initialization of objects.
+//    ii) Your property will not be initialized unless you use it.
+//    iii) It is initialized only once. Next time when you use it, you get the value from cache memory.
+//    iv) lazy { ... } delegate can only be used for val properties
     private val auth by lazy {
         FirebaseAuth.getInstance()
     }
